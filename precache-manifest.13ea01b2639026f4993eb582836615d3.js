887cc9ad11ac73fda0a487d2901e3a93a72674e2
iasjem <jemimah.beryl.sai@gmail.com>
2020-02-23 19:37:13 +0800

    Updates

---
+++ precache-manifest.13ea01b2639026f4993eb582836615d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265a934102cf512715e1aa7eea679293",
+    "url": "/notesbin/index.html"
+  },
+  {
+    "revision": "d0da94548389b160bdc8",
+    "url": "/notesbin/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f48d850938ad3726d233",
+    "url": "/notesbin/static/js/2.4a0c343e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/notesbin/static/js/2.4a0c343e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0da94548389b160bdc8",
+    "url": "/notesbin/static/js/main.9aa8b6fb.chunk.js"
+  },
+  {
+    "revision": "f94def5ea527e0b25ed8",
+    "url": "/notesbin/static/js/runtime-main.33ab68fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/notesbin/static/media/logo.5d5d9eef.svg"
+  }
+]);
