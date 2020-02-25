772a66a2b531ba393c9996171334b53078d4303f
iasjem <jemimah.beryl.sai@gmail.com>
2020-02-25 12:51:42 +0800

    Add a container for main content of app - /setup_ui

---
+++ src/Content.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Content.css';
+
+function Content() {
+    return (
+        <div class="Content">
+            <main className="Main-content">
+                
+            </main>
+        </div>
+    );
+}
+
+export default Content;
