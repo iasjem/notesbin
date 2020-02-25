46e168c85ffe9f2157df7be7338d12ddf4946f42
iasjem <jemimah.beryl.sai@gmail.com>
2020-02-25 13:20:38 +0800

    Add notice message for app under development - /ui_content

--- src/Content.js
+++ src/Content.js
@@ -5,7 +5,10 @@ function Content() {
     return (
         <div class="Content">
             <main className="Main-content">
-                
+                <div className="Notice-message">
+                    <h1>App Under Development</h1>
+                    <p>Please stay tune and check out the <a href="https://github.com/iasjem/notesbin">repository</a> for more updates!</p>
+                </div>
             </main>
         </div>
     );
