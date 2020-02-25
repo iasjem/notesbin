a8f4895576b1a54f1986ada46424282251355325
iasjem <jemimah.beryl.sai@gmail.com>
2020-02-25 12:54:05 +0800

    Add Header and Content components to App - /setup_ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Content from './Content';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Content />
     </div>
   );
 }
