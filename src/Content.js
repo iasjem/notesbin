06a125f3c33e5c511d335d5b9a035934cd65cc22
iasjem <jemimah.beryl.sai@gmail.com>
2020-02-28 21:53:12 +0800

    Move code for displaying notice message to Notice component - /setup_ui

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './Content.css';
+import Notice from './Notice';
 
 function Content() {
     return (
         <div class="Content">
             <main className="Main-content">
-                <div className="Notice-message">
-                    <h1>App Under Development</h1>
-                    <p>Please stay tune and check out the <a href="https://github.com/iasjem/notesbin">repository</a> for more updates!</p>
-                </div>
+                <Notice />
             </main>
         </div>
     );
